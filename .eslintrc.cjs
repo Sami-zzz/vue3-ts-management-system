ec1f7685cd8fa19fa50b1de5126df31a4832df9c
Sami-zzz <2787621724@qq.com>
2023-05-08 21:43:57 +0800

    feat: 项目路由配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,8 +13,10 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest'
   },
-
   env: {
     node: true
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off'
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
+  <div class="app">
     <h2>app</h2>
+    <RouterLink to="/login">登录</RouterLink>
+    <RouterLink to="/main">主页</RouterLink>
+    <RouterView></RouterView>
   </div>
 </template>
 
