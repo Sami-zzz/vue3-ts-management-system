34d570bc04d43e21947281a1894ac022c1edff4e
Sami-zzz <2787621724@qq.com>
2023-05-15 20:56:58 +0800

    feat: 主页结构搭建，菜单静态实现

---
+++ src/components/mainHeader/MainHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="mainHeader">
+    <h2>mainHeader</h2>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped></style>
--- src/views/main/Main.vue
+++ src/views/main/Main.vue
@@ -1,19 +1,51 @@
 <template>
   <div class="main">
-    <h2>main</h2>
-    <button @click="handleExitClick">退出登录</button>
+    <el-container class="main-content">
+      <el-aside width="210px">
+        <MainMenu />
+      </el-aside>
+      <el-container>
+        <el-header height="50px">
+          <MainHeader />
+        </el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script setup lang="ts">
-import { LOGIN_TOKEN } from '@/global/constants'
-import { localCache } from '@/utils/cache'
-import { useRouter } from 'vue-router'
-const router = useRouter()
-const handleExitClick = () => {
-  localCache.deleteCache(LOGIN_TOKEN)
-  router.push('/login')
-}
+import MainHeader from '@/components/mainHeader/MainHeader.vue'
+import MainMenu from '@/components/mainMenu/MainMenu.vue'
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.main {
+  height: 100%;
+}
+
+.main-content {
+  height: 100%;
+
+  .el-aside {
+    overflow-x: hidden;
+    overflow-y: auto;
+    line-height: 200px;
+    text-align: left;
+    cursor: pointer;
+    background-color: #001529;
+    scrollbar-width: none; /* firefox */
+    -ms-overflow-style: none; /* IE 10+ */
+
+    transition: width 0.3s ease;
+
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+
+  .el-main {
+    background-color: #f0f2f5;
+  }
+}
+</style>
